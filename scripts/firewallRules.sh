0975f6305d47fbf27333c890b5c10956feef8feb
oriveraTigabytes <oscar@tigabytes.com>
2020-02-26 17:25:54 -0300

    add port

--- scripts/firewallRules.sh
+++ scripts/firewallRules.sh
@@ -1,5 +1,5 @@
 gcloud compute firewall-rules create default-allow-http-80 \
-    --allow tcp:80,8000 \
+    --allow tcp:8000 \
     --source-ranges 0.0.0.0/0 \
     --target-tags http-server \
     --description "Allow port 80 access to http-server"
