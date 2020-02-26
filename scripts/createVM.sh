16e39ad12dea52cc20982b941ac5c66a8cff219f
oriveraTigabytes <oscar@tigabytes.com>
2020-02-26 13:16:26 -0300

    scripts

---
+++ scripts/createVM.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+gcloud compute instances create my-app-instance \
+    --image-family=debian-9 \
+    --image-project=debian-cloud \
+    --machine-type=g1-small \
+    --scopes userinfo-email,cloud-platform \
+    --metadata app-location=$DEPLOY_LOCATION \
+    --metadata-from-file startup-script=gce/startup-script.sh \
+    --zone us-central1-f \
+    --tags http-server
