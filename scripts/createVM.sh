6f461822013dd6a3bacaa6c9e0d9696823692262
oriveraTigabytes <oscar@tigabytes.com>
2020-02-26 13:47:20 -0300

    node app

--- scripts/createVM.sh
+++ scripts/createVM.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
+PROJECT=$(gcloud info --format='value(config.project)')
+
 gcloud compute instances create my-app-instance \
     --image-family=debian-9 \
     --image-project=debian-cloud \
     --machine-type=g1-small \
     --scopes userinfo-email,cloud-platform \
     --metadata app-location=$DEPLOY_LOCATION \
-    --metadata-from-file startup-script=gce/startupscript.sh \
+    --metadata-from-file startup-script=startupscript.sh \
     --zone us-central1-f \
     --tags http-server
---
+++ scripts/firewallRules.sh
@@ -0,0 +1,5 @@
+gcloud compute firewall-rules create default-allow-http-80 \
+    --allow tcp:80 \
+    --source-ranges 0.0.0.0/0 \
+    --target-tags http-server \
+    --description "Allow port 80 access to http-server"
