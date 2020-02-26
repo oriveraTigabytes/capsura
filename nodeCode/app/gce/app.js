e08377eff0f418379e40149db6b718c427effa04
oriveraTigabytes <oscar@tigabytes.com>
2020-02-26 16:35:05 -0300

    cambios app node

--- nodeCode/app/gce/app.js
+++ nodeCode/app/gce/app.js
@@ -22,12 +22,12 @@ const app = express();
 app.get('/', (req, res) => {
   res
     .status(200)
-    .send('Hello, world!')
+    .send('Hello, world Sura!')
     .end();
 });
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
